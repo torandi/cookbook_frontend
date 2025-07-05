837e49d80bfbbb58e67973cdc37b34997e6952cb
Andreas Tarandi <torandi@gmail.com>
2025-07-05 17:01:06 +0200

    support for injecting steps

--- app/recipe/add/ingredients.tsx
+++ app/recipe/add/ingredients.tsx
@@ -17,6 +17,7 @@ import CircularProgress from '@mui/material/CircularProgress';
 import FormControl from '@mui/material/FormControl';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
+import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
 import InputAdornment from '@mui/material/InputAdornment';
 import InputLabel from '@mui/material/InputLabel';
@@ -231,13 +232,15 @@ function IngredientEntryInput({ id, ingredientsState, setIngredientsState } : {
 						}}
 						className="flex-2"
 					/>
-					<IconButton
-						className="flex-none self-center justify-self-end"
-						onClick={handleDelete}
-						tabIndex="-1"
-					>
-						<DeleteIcon/>
-					</IconButton>
+					<Tooltip title="Ta bort ingrediens">
+						<IconButton
+							className="flex-none self-center justify-self-end"
+							onClick={handleDelete}
+							tabIndex="-1"
+						>
+							<DeleteIcon/>
+						</IconButton>
+					</Tooltip>
 				</Box>
 			</FormControl>
 			<IngredientCreateDialog
--- app/recipe/add/instructions.tsx
+++ app/recipe/add/instructions.tsx
@@ -7,9 +7,11 @@ import FormControl from '@mui/material/FormControl';
 import InputAdornment from '@mui/material/InputAdornment';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
+import Tooltip from '@mui/material/Tooltip';
 import IconButton from '@mui/material/IconButton'
 
 import DeleteIcon from '@mui/icons-material/Delete'
+import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd'
 
 interface State {
 	nextId: number,
@@ -25,8 +27,10 @@ function InstructionStepInput({ id, index, instructionsState, setInstructionsSta
 	}) {
 	const [value, setValue] = useState("");
 
+	const isLastInstruction = instructionsState.activeIds.at(-1) == id);
+
 	const handleDelete = () => {
-		if(instructionsState.activeIds.at(-1) == id)
+		if(isLastInstruction)
 		{
 			// We're the last item, just clear our value
 			// (probably won't happen, but best to keep it anyway)
@@ -41,7 +45,13 @@ function InstructionStepInput({ id, index, instructionsState, setInstructionsSta
 		}
 	}
 
-	// todo: stöd för att lägga till instruktion mitt i
+	const injectStep = () => {
+		const newId = instructionsState.nextId;
+		setInstructionsState({
+			nextId: newId + 1,
+			activeIds: instructionsState.activeIds.toSpliced(index+1, 0, newId)
+		})
+	}
 
 	return (
 		<Box className="w-full flex flex-row">
@@ -51,7 +61,7 @@ function InstructionStepInput({ id, index, instructionsState, setInstructionsSta
 			className="flex-3"
 			value={value}
 			onChange={ (event: ChangeEvent ) => {
-				if(event.target.value && instructionsState.activeIds.at(-1) == id)
+				if(event.target.value && isLastInstruction)
 				{
 					// We are the last item, add new
 					const newId = instructionsState.nextId;
@@ -72,16 +82,36 @@ function InstructionStepInput({ id, index, instructionsState, setInstructionsSta
 				}
 			}}
 			/>
-			<IconButton
-				onClick={handleDelete}
+			<Stack
 				className="flex-none self-center justify-self-end"
-				tabIndex="-1"
+				direction="column"
 			>
-				<DeleteIcon/>
-			</IconButton>
+				<Tooltip title="Ta bort steg">
+					<IconButton
+						onClick={handleDelete}
+						tabIndex="-1"
+						>
+						<DeleteIcon/>
+					</IconButton>
+				</Tooltip>
+				{
+					!isLastInstruction ? (
+						<Tooltip title="Lägg till steg efter">
+							<IconButton
+								onClick={injectStep}
+								tabIndex="-1"
+								>
+								<PlaylistAddIcon/>
+							</IconButton>
+						</Tooltip>
+						) : (<></>)
+				}
+			</Stack>
 		</Box>
 	)
 }
+/*
+					*/
 
 const InstructionsInput = () => {
 	const [instructionsState, setInstructionsState] = useState<State>({
